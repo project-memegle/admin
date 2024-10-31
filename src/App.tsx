786b03fb2253f50db146fbc6e69f06bede184960
한지희, Jihee Han <hatrix1014@gmail.com>
2024-10-31 20:44:12 +0900

    Setup responsive css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from './components/UI/NavBar/NavBar';
+import Header from './components/UI/Header/Header';
 function App() {
     return (
         <div className="body__container">
+            <Header />
             <NavBar />
             <Outlet />
         </div>
--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -1,13 +1,57 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
     const navigate = useNavigate();
-    function navigateToLogIn() {
-        navigate('/login');
+    const [selectedMenu, setSelectedMenu] = useState<string | null>(null);
+
+    function navigateTo(path: string, menu: string) {
+        navigate(path);
+        setSelectedMenu(menu);
     }
     return (
-        <header className='c-header'>
-            <button onClick={navigateToLogIn}>관리자 로그인</button>
+        <header className="c-header">
+            <button onClick={() => navigateTo('/', 'home')}>
+                <img className="logo" src="/src/assets/logo.svg" alt="logo" />
+            </button>
+            <ul>
+                <li
+                    className={`c-navbar__list ${
+                        selectedMenu === 'image' ? 'selected' : ''
+                    }`}
+                >
+                    <button onClick={() => navigateTo('/image', 'image')}>
+                        <div className="c-navbar__list-category">이미지</div>
+                    </button>
+                </li>
+                <li
+                    className={`c-navbar__list ${
+                        selectedMenu === 'user' ? 'selected' : ''
+                    }`}
+                >
+                    <button onClick={() => navigateTo('/user', 'user')}>
+                        <div className="c-navbar__list-category">회원</div>
+                    </button>
+                </li>
+                <li
+                    className={`c-navbar__list ${
+                        selectedMenu === 'chat' ? 'selected' : ''
+                    }`}
+                >
+                    <button onClick={() => navigateTo('/chat', 'chat')}>
+                        <div className="c-navbar__list-category">문의</div>
+                    </button>
+                </li>
+                <li
+                    className={`c-navbar__list ${
+                        selectedMenu === 'category' ? 'selected' : ''
+                    }`}
+                >
+                    <button onClick={() => navigateTo('/category', 'category')}>
+                        <div className="c-navbar__list-category">카테고리</div>
+                    </button>
+                </li>
+            </ul>
         </header>
     );
 }
