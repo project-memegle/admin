ca7dab4f5b06c2b53d1b7ff24b80ac6ee6df993a
한지희, Jihee Han <hatrix1014@gmail.com>
2024-11-01 05:13:50 +0900

    resolved legacy error

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 import '@memegle/styles';
 
 import router from './Router'; // Import default export
