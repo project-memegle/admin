9bb09825e7075b12f3149d6e9a1bfeeb4b9c3533
한지희, Jihee Han <hatrix1014@gmail.com>
2024-12-13 05:31:47 +0900

    Refoctored formatDate

--- src/utils/Format/formatDate.ts
+++ src/utils/Format/formatDate.ts
@@ -3,32 +3,26 @@ import { format, isToday, isYesterday, differenceInDays } from 'date-fns';
 export function formatDate(dateString: string, language: string): string {
     const date = new Date(dateString);
     const now = new Date();
+    let formattedDate = '';
 
-    if (language === 'ko') {
-        if (isToday(date)) {
-            return format(date, 'HH:mm');
-        } else if (isYesterday(date)) {
-            return '어제 ' + format(date, 'HH:mm');
-        } else if (differenceInDays(now, date) < 1) {
-            return format(date, 'HH:mm');
-        } else if (differenceInDays(now, date) < 365) {
-            return format(date, 'M월 d일');
-        } else {
-            return format(date, 'yyyy년 M월 d일');
-        }
+    if (isToday(date)) {
+        formattedDate = format(date, 'HH:mm');
+    } else if (isYesterday(date)) {
+        formattedDate =
+            (language === 'ko' ? '어제 ' : 'Yesterday ') +
+            format(date, 'HH:mm');
+    } else if (differenceInDays(now, date) < 1) {
+        formattedDate = format(date, 'HH:mm');
+    } else if (differenceInDays(now, date) < 365) {
+        formattedDate = format(date, language === 'ko' ? 'M월 d일' : 'MMM d');
     } else {
-        if (isToday(date)) {
-            return format(date, 'HH:mm');
-        } else if (isYesterday(date)) {
-            return 'Yesterday ' + format(date, 'HH:mm');
-        } else if (differenceInDays(now, date) < 1) {
-            return format(date, 'HH:mm');
-        } else if (differenceInDays(now, date) < 365) {
-            return format(date, 'MMM d');
-        } else {
-            return format(date, 'yyyy MMM d');
-        }
+        formattedDate = format(
+            date,
+            language === 'ko' ? 'yyyy년 M월 d일' : 'yyyy MMM d'
+        );
     }
+
+    return formattedDate;
 }
 
 export function formatStaticDate(dateString: string, language: string): string {
